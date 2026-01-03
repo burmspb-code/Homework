443e33c3654e117716d7f5ca5001b6568e487298
Mikhail Byrdukovsky <burmspb@gmail.com>
2026-01-03 21:53:54 +0300

    deleting comments in masks.py

--- src/masks.py
+++ src/masks.py
@@ -17,8 +17,3 @@ def get_mask_account(account_n: int) -> str:
     return str(resalt)
 
 
-card_number = 7000792289606361
-account_number = 73654108430135874305
-
-print(get_mask_card_number(card_number))
-print(get_mask_account(account_number))
