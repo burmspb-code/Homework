d7914d7ae759383ea339800024fa59406531b327
Mikhail Burdukovsky <burmspb@gmail.com>
2026-01-21 21:47:34 +0300

    adding the processing module and initializing functions

---
+++ src/processing.py
@@ -0,0 +1,9 @@
+"""Функции обработки данных"""
+
+def filter_by_state(list_dict: list[dict], key: str = "EXECUTED") -> list[dict]:
+    """Возвращает новый список словарей по ключу"""
+    pass
+
+def sort_by_date(list_dict: list[dict], key_sort: bool = True) -> list[dict]:
+    """Возвращает новый список, отсортированный по дате"""
+    pass
