df5292d27ddbf9d0b5f2d0c6a4eae7b05009950f
Mikhail Byrdukovsky <burmspb@gmail.com>
2026-01-24 20:19:22 +0300

    checking with linters

--- src/processing.py
+++ src/processing.py
@@ -7,13 +7,13 @@ def filter_by_state(list_dicts: list[dict], key: str = "EXECUTED") -> list[dict]
     return [item for item in list_dicts if item["state"] == key]
 
 
-def sort_by_date(list_dicts: list[dict], key_sort: bool = True) -> list[dict]:
+def sort_by_date(list_dicts: list[dict], reverse: bool = True) -> list[dict]:
     """Возвращает новый список, отсортированный по дате
-    key_sort = True - по удыванию (по умолчанию)
-    key_sort = False - по возрастанию
+    reverse = True - по убыванию (по умолчанию)
+    reverse = False - по возрастанию
     """
 
-    return sorted(list_dicts, key=lambda x: x["date"], reverse=key_sort)
+    return sorted(list_dicts, key=lambda x: x["date"], reverse=reverse)
 
 
 # Проверка filter_by_state
@@ -23,7 +23,7 @@ def sort_by_date(list_dicts: list[dict], key_sort: bool = True) -> list[dict]:
 #           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 #
 # print(filter_by_state(list_d))
-# print(filter_by_state(list_d, 'CANCELED'))
+# #print(filter_by_state(list_d, 'CANCELED'))
 
 # Проверка sort_by_date
 # list_d = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -31,5 +31,5 @@ def sort_by_date(list_dicts: list[dict], key_sort: bool = True) -> list[dict]:
 #           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 #           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 #
-# #print(sort_by_date(list_d)
-# print(sort_by_date(list_d, False))
+# print(sort_by_date(list_d))
+# #print(sort_by_date(list_d, False))
