6e9b2c803aee076c601e02d34a288525e79184a9
Mikhail Burdukovsky <burmspb@gmail.com>
2026-01-26 21:00:13 +0300

    Revert "checking with linters"
    This reverts commit b30b1dffdaf88353539dff360f5998a7a4b54fa2.

--- src/processing.py
+++ src/processing.py
@@ -15,6 +15,16 @@ def sort_by_date(list_dicts: list[dict], reverse: bool = True) -> list[dict]:
 
     return sorted(list_dicts, key=lambda x: x["date"], reverse=reverse)
 
+
+# Проверка filter_by_state
+# list_d = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#
+# print(filter_by_state(list_d))
+# #print(filter_by_state(list_d, 'CANCELED'))
+
 # Проверка sort_by_date
 # list_d = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 #           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
