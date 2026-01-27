5ae82dd5796cda9a39f613cb199c69aa9a73e40f
Mikhail Burdukovsky <burmspb@gmail.com>
2026-01-27 22:58:20 +0300

    update testing the module processing

--- test/test_processing.py
+++ test/test_processing.py
@@ -1,33 +1,29 @@
+"""Тестирование функций filter_by_state, sort_by_date"""
+
+import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
-def test_filter_by_state():
-    list_d = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-    assert filter_by_state(list_d) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-                                       ]
-    assert filter_by_state(list_d, 'CANCELED') == [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-                                                        ]
-    assert filter_by_state(list_d, 'EXECUTED') == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-                                                        ]
+
+@pytest.mark.parametrize(
+    "list_input_data, key_status, expected_result",
+    [
+        ("list_input_data", "EXECUTED", "list_expected_data_executed"),
+        ("list_input_data", "CANCELED", "list_expected_data_canceled"),
+    ],
+    indirect=["list_input_data", "expected_result"],
+)
+def test_filter_by_state(list_input_data, key_status, expected_result):
+    assert filter_by_state(list_input_data, key_status) == expected_result
 
 
-def test_sort_by_date():
-    list_d = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-    assert sort_by_date(list_d) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-    assert sort_by_date(list_d, False) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                                    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+@pytest.mark.parametrize(
+    "list_input_data, reverse, expected_result",
+    [
+        ("list_input_data", True, "list_expected_data_is_reverse"),
+        ("list_input_data", False, "list_expected_data_is_not_reverse"),
+    ],
+    indirect=["list_input_data", "expected_result"],
+)
+def test_sort_by_date(list_input_data, reverse, expected_result):
+    assert sort_by_date(list_input_data, reverse) == expected_result
