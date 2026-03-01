0f3a872ef7aaa0e6147be6fc576dc6a5c5a5b7ce
Mikhail Byrdukovsky <burmspb@gmail.com>
2026-03-01 19:47:35 +0300

    logging completed

--- src/masks.py
+++ src/masks.py
@@ -1,23 +1,41 @@
 """Наложение маски на номер банковской карты и номер банковского счета"""
 
 from src.create_masks import get_mask
+from src.logger.config import setup_logger
+
+# Создаем объект логера для masks
+logger = setup_logger("masks")
 
 
 def get_mask_card_number(card_n: int) -> str:
     """Принимает на вход номер карты в виде числа и возвращает маску номера по правилу
     XXXX XX** **** XXXX"""
+
+    logger.debug(f"Маскировка номера карты: {card_n}")
+
     if not isinstance(card_n, int):
+        logger.error(f"Ошибка - номер карты {card_n} не целое число")
+
         raise TypeError("Номер карты должен быть целым числом")
 
     result = get_mask(str(card_n), 6, 11, "*", 4, " ")
+    logger.info("Номер карты успешно замаскирован")
+
     return str(result)
 
 
 def get_mask_account(account_n: int) -> str:
-    """Принимает на вход номер счета в виде числа и возвращает маску номера по правилу
+    """Принимает на вход номер счета в виде числа и возвращает маску номера счета по правилу
     **XXXX"""
+
+    logger.debug(f"Маскиовка номера счета: {account_n}")
+
     if not isinstance(account_n, int):
+        logger.error(f"Ошибка - номер счета {account_n} не целое число")
+
         raise TypeError("Номер счета должен быть целым числом")
 
     result = get_mask(str(account_n), 0, 15, "*", 0, "")[-6::]
+    logger.info("Номер счета успешно замаскирован")
+
     return str(result)
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,16 @@
 import json
 from pathlib import Path
 
+from src.logger.config import setup_logger
+
+# Создаем объект логера для utils
+logger = setup_logger("utils")
+
 
 def get_json_data(path: str) -> list[dict]:
     """Возвращает список словарей с данными о финансовых транзакциях."""
+
+    logger.debug(f"Чтение данных json из файла {path}")
 
     current_dir = Path(__file__).parent  # Получаем текущую директорию.
     file_path = current_dir.parent / path  # Поднимаемся на уровень выше и идем в path
@@ -12,7 +19,17 @@ def get_json_data(path: str) -> list[dict]:
         with open(file_path) as file:
             data = json.load(file)
         if not isinstance(data, list):
+            logger.error("Файл должен содержать список объектов")
             data = []
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
+
+    except FileNotFoundError:
+        logger.error(f"Ошибка - файл {path} не найден")
         data = []
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON в файле: {path}")
+        data = []
+
+    if data:
+        logger.info("Данные успешно загружены")
+
     return data
